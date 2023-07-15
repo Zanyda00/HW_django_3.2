ef8b8a130841d8cd609f61a1d1bedc45aa8f89d6
Igor Stepurko <stepurkovaychok@gmail.com>
2023-07-15 21:15:39 +0300

    first commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,22 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.order_by('title')
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.order_by('address')
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['products__title']
